aa8574751060a5623260f75f480d1b967ca66df6
Hendrik Luehrsen <hendrik@luehrsen.de>
2020-05-02 18:12:21 +0200

    Include the config in the repo

---
+++ xenforo/src/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config['db']['host'] = 'xf2-database';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'homestead';
+$config['db']['password'] = 'secret';
+$config['db']['dbname'] = 'homestead';
+
+$config['fullUnicode'] = true;
+
+$config['debug'] = true;
+$config['development']['enabled'] = true;
+$config['development']['defaultAddOn'] = 'LH/CookieConsent';
+$config['enableLivePayments'] = false;
+$config['designer']['enabled'] = true;
